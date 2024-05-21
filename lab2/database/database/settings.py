6aa06612da2e90a6283f7accb1950bab279cc0cd
Yiran <1378468839@qq.com>
2024-05-21 22:42:55 +0800

    server app init

--- lab2/database/database/settings.py
+++ lab2/database/database/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'server',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
