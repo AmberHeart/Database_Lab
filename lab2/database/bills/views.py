bebeec66370db1c8b63ab32ca9718b0f70bf1615
Yiran <1378468839@qq.com>
2024-05-30 11:34:44 +0800

    report to be done

--- lab2/database/bills/views.py
+++ lab2/database/bills/views.py
@@ -12,31 +12,38 @@ from users.models import BankUser
 @login_required
 def create_bill(request, account_id):
     account = UserAccounts.objects.get(account_id=account_id)
-    # judge if the user is the owner of the account
     user_id = BankUser.objects.get(id=account.user_id).user_id
     if request.user.id != user_id:
         messages.warning(request, '无法为他人账户创建账单')
         return render(request, 'frontend/error.html')
+    
     if request.method != 'POST':
         form = AccountBillsForm(initial={'account': account})
     else:
         form = AccountBillsForm(initial={'account': account}, data=request.POST)
         if form.is_valid():
-            old_money = account.money
             changes = form.cleaned_data.get("changes")
             bill_type = form.cleaned_data.get("type")
-            remark = form.cleaned_data.get("remark")
-            new_money = old_money + changes
-            if new_money < 0:
-                messages.warning(request, '余额不足')
-                return render(request, 'frontend/error.html')
-            bill = AccountBills.objects.create(account=account, changes=changes,
-                                               type=bill_type, remark=remark, money=new_money)
-            bill.save()
-            # 触发器，自动更新账户余额
-            account.money = new_money
-            account.save()
-            return redirect('bills:bills', account_id=account.account_id)
+            
+            # 验证账单类型和金额
+            if bill_type == '收入' and changes <= 0:
+                messages.warning(request, '收入类型的账单金额必须大于0')
+            elif bill_type == '支出' and changes >= 0:
+                messages.warning(request, '支出类型的账单金额必须小于0')
+            else:
+                old_money = account.money
+                new_money = old_money + changes
+                if new_money < 0:
+                    messages.warning(request, '余额不足')
+                    return render(request, 'frontend/error.html')
+                
+                bill = AccountBills.objects.create(account=account, changes=changes,
+                                                   type=bill_type, remark=form.cleaned_data.get("remark"), money=new_money)
+                bill.save()
+                account.money = new_money
+                account.save()
+                return redirect('bills:bills', account_id=account.account_id)
+    
     context = {'form': form, 'account': account}
     return render(request, 'bills/create_bill.html', context)
 
